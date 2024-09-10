20d8c4912467b4cb8d7de159478ce60e0f00c717
willington lucas <lucaswillinngton598@gmail.com>
2024-09-09 21:37:34 -0300

    subindo codigo base

---
+++ projeto_funcoes_polinomiais.py
@@ -0,0 +1,15 @@
+
+
+print("este programa visa auxiliar aluno na resolução de funções do primeiro grau\n aqui você encontrarã diversos tipos de funções")
+funcao_def=str.lower(input("primeiramente, voce sabe o que é uma função?\n sim ou não"))
+if funcao_def=="nao" or funcao_def=="não" or funcao_def=="n":
+    print("uma função é uma relação entre duas grandezas ou mais etcetc etc")
+else:
+    print("okay, vamos prosseguir")
+tipo_funcao=int(input("por favor, nforme-me o tipo de função que deseja trabalhar"))
+
+
+
+
+if tipo_funcao=="funcao quadratrica":
+    print("vc escolheu afuncão quadratica")
